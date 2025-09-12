2b757a2130386acb2f99017b13a2609055986a4f
anto <aigrandon@miuandes.cl>
2025-09-11 23:33:42 -0300

    listo

--- config/routes.rb
+++ config/routes.rb
@@ -12,12 +12,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  get '/rooms', to: 'rooms#index', as: :rooms
+  get "/rooms", to: "rooms#index", as: :rooms
   get "/rooms/:id", to:"rooms#show", as: :room
-  get '/guests', to: 'guests#index', as: :guests
-  get '/guests/:id', to: 'guests#show', as: :guest
-  get '/reservations', to: 'reservations#index', as: :reservations
-  get '/reservations/:id', to: 'reservations#show', as: :reservation
-  get '/services', to: 'services#index', as: :services
-  get '/services/:id', to: 'services#show', as: :service
+  get "/guests", to: "guests#index", as: :guests
+  get "/guests/:id", to: "guests#show", as: :guest
+  get "/reservations", to: "reservations#index", as: :reservations
+  get "/reservations/:id", to: "reservations#show", as: :reservation
+  get "/services", to: "services#index", as: :services
+  get "/services/:id", to: "services#show", as: :service
 end
